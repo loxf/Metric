3bcb367f749c9e59d10f13644537fb614e6049b3
luohj <myself35335@163.com>
2017-07-05 23:02:12 +0800

    modify

--- metric-core/src/main/java/org/loxf/metric/core/mongo/MongoDaoBase.java
+++ metric-core/src/main/java/org/loxf/metric/core/mongo/MongoDaoBase.java
@@ -16,78 +16,70 @@ import java.util.Map;
 /**
  * Created by luohj on 2017/6/27.
  */
-public class MongoDaoBase<T> implements MongoBase<T> {
+public class MongoDaoBase<T> {
     private static final String SET = "$set";
     private static final String INC = "$inc";
     private static final String PUSH = "$push";
     @Resource
     protected MongoTemplate mongoTemplate;
 
-    @Override
     public void insert(T object, String collectionName) {
         mongoTemplate.insert(object, collectionName);
     }
 
-    @Override
     public T findOne(Map<String, Object> params, String collectionName) {
         DBObject obj = new BasicDBObject();
         obj.putAll(params);
-        Query query=new BasicQuery(obj);
-        Class < T >  clazz  =  (Class < T > ) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[ 0 ];
+        Query query = new BasicQuery(obj);
+        Class<T> clazz = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
         return mongoTemplate.findOne(query, clazz, collectionName);
     }
 
-    @Override
     public List<T> findAll(Map<String, Object> params, String collectionName) {
         DBObject obj = new BasicDBObject();
         obj.putAll(params);
-        Query query=new BasicQuery(obj);
-        Class < T >  clazz  =  (Class < T > ) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[ 0 ];
+        Query query = new BasicQuery(obj);
+        Class<T> clazz = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
         return mongoTemplate.find(query, clazz, collectionName);
     }
 
-    @Override
     public List<T> findByPager(Map<String, Object> params, int start, int end, String collectionName) {
         DBObject obj = new BasicDBObject();
         obj.putAll(params);
-        Query query=new BasicQuery(obj);
+        Query query = new BasicQuery(obj);
         query.skip(start).limit(end);
-        Class < T >  clazz  =  (Class < T > ) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[ 0 ];
+        Class<T> clazz = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
         return mongoTemplate.find(query, clazz, collectionName);
     }
 
-    @Override
     public void update(Map<String, Object> queryParams, Map<String, Object> setParams, String collectionName) {
         DBObject queryObj = new BasicDBObject();
         queryObj.putAll(queryParams);
-        Query query=new BasicQuery(queryObj);
+        Query query = new BasicQuery(queryObj);
         BasicDBObject setObj = new BasicDBObject();
         setObj.put(SET, new BasicDBObject(setParams));
         Update update = new BasicUpdate(setObj);
         mongoTemplate.updateMulti(query, update, collectionName);
     }
 
-    @Override
-    public void updateOne(Map<String, Object> queryParams, Map<String, Object> setParams, String collectionName){
+    public void updateOne(Map<String, Object> queryParams, Map<String, Object> setParams, String collectionName) {
         DBObject queryObj = new BasicDBObject();
         queryObj.putAll(queryParams);
-        Query query=new BasicQuery(queryObj);
+        Query query = new BasicQuery(queryObj);
         BasicDBObject setObj = new BasicDBObject();
         setObj.put(SET, new BasicDBObject(setParams));
         Update update = new BasicUpdate(setObj);
         mongoTemplate.updateFirst(query, update, collectionName);
     }
 
-    @Override
     public void createCollection(String collectionName) {
         mongoTemplate.createCollection(collectionName);
     }
 
-    @Override
     public void remove(Map<String, Object> params, String collectionName) {
         DBObject obj = new BasicDBObject();
         obj.putAll(params);
-        Query query=new BasicQuery(obj);
+        Query query = new BasicQuery(obj);
         mongoTemplate.remove(query, collectionName);
     }
 }
