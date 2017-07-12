fba0a2a439eab480e26dc29ceb5e9a2235b10961
tingting.hu <tingting.hu@tongdun.cn>
2017-07-12 20:52:57 +0800

    基础分页更改

--- metric-api/src/main/java/org/loxf/metric/api/IBaseService.java
+++ metric-api/src/main/java/org/loxf/metric/api/IBaseService.java
@@ -3,13 +3,11 @@ package org.loxf.metric.api;
 import org.loxf.metric.common.dto.BaseResult;
 import org.loxf.metric.common.dto.PageData;
 
-import java.util.Map;
-
 /**
  * Created by hutingting on 2017/7/7.
  */
 public interface IBaseService<T> {
-    public PageData getPageList(T obj);
+    public BaseResult<PageData> getPageList(T obj);
     public BaseResult<String> insertItem(T obj);
     public BaseResult<T> queryItemByCode(String itemCode,String handleUserName);
     public BaseResult<String> updateItem(T obj);
